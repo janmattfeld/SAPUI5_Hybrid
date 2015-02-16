1ce5fb115c806640e81f757bc5ab93c244697ff7
janmattfeld <dev-github@janmattfeld.de>
2015-02-16 15:16:21 +0100

    FIX Delegate OnBeforeShow

--- www/view/Camera.controller.js
+++ www/view/Camera.controller.js
@@ -5,6 +5,14 @@ view: null,
 src: null,
 text: null,
 
+	onInit: function() { 
+	//to register for beforeShow event 
+		this.getView().addEventDelegate({
+			// not added the controller as delegate to avoid controller functions with similar names as the events 
+			onBeforeShow : jQuery.proxy(function(evt) { this.onBeforeShow(evt); }, this) 
+		}); 
+	},
+
 	onBeforeShow: function() {
 	    // Set app
         this.app = sap.ui.getCore().byId("idMyApp");
